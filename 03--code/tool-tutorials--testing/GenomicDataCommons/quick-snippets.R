f12718b03f91987738ad4b140fa5551365efbcdc
Leo Williams <leowill3@icloud.com>
2019-12-10 15:35:23 -0600

    Testing making GDC ID conversion table from GDC-R results

--- 03--code/tool-tutorials--testing/GenomicDataCommons/quick-snippets.R
+++ 03--code/tool-tutorials--testing/GenomicDataCommons/quick-snippets.R
@@ -12,3 +12,12 @@ cases() %>%
 	select(c("submitter_id", "submitter_sample_ids", "samples.sample_id")) %>% 
 	results()
 
+fileID_caseID_table = tibble(file_UUID = NA, 
+							 TCGA_case_ID = NA)
+for (i in 1:length(q_files_uuid$cases)) {
+	print(q_files_uuid$file_id[i])
+	print(q_files_uuid$cases[[i]])
+	
+	fileID_caseID_table[i, "file_UUID"] = q_files_uuid$file_id[i]
+	fileID_caseID_table[i, "TCGA_case_ID"] = q_files_uuid$cases[[i]][[1]]
+}
