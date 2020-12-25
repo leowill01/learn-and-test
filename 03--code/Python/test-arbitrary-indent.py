4a2b559cea5b2a4c8d3a8d87917ae71957a49743
Leo Williams <leowill01@icloud.com>
2020-12-24 22:57:16 -0700

    Create test-arbitrary-indent.py

---
+++ 03--code/Python/test-arbitrary-indent.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+# Python is indentation-aware. you cant arbitrarily indent code in Python just to make it easier to read
+
+# %% 1. test indentation
+	friends = ['Marco','Holden','Naomi','Amos','Alex']
+print(friends)
+
+# %% 2 test indentation
+	for friend in friends:
+		print('One friend is ' + friend)
